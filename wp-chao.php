28e3a58e83d92e9eeec5ee44f088cef825d2dad1
Xingchao Zhang <xingchaovv@gmail.com>
2020-12-26 11:37:55 +0800

    更新插件说明。

--- wp-chao.php
+++ wp-chao.php
@@ -1,7 +1,11 @@
 <?php
 /**
  * Plugin Name: WP Chao
- * Description: WP Chao 是一款 WordPress 的插件。目前主要用于笔者个人博客，欢迎参考。
+ * Description: WP Chao 是一款 WordPress 的插件。目前主要用于笔者个人博客，欢迎参考、使用。
+ * Version: 1.0
+ * Plugin URI: https://github.com/xingchaovv/wp-chao
+ * Author: Xingchao Zhang
+ * Author URI: http://xingchaovv.com
  */
 
 require __DIR__ . '/util/Read.php';
